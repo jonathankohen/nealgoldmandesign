7e107dfb8ce9090d432cc604b8d88f8bf807124a
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2023-07-08 21:10:00 -0400

    Added second part of home page.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 $(function () {
-	$(window).on("resize", function () {
-		const win = $(this);
-		if (win.width() <= 992) {
+	function checkWidth() {
+		if ($(window).width() <= 992) {
 			$("#hero__img")
 				.insertAfter("#hero__blurb")
 				.css("display", "flex")
 				.css("flex-direction", "column")
 				.css("align-items", "center")
 				.css("margin-top", "2em");
-		}
-
-		if (win.width() > 992) {
+		} else {
 			$("#hero__img").insertAfter("#hero__text");
 		}
+
+		// if ($(window).width() <= 1360) {
+		// 	$("#experiences").css("color", "#e5e2d7");
+		// } else {
+		// 	$("#experiences").css("color", "#bc6c25");
+		// }
+	}
+
+	checkWidth();
+
+	$(window).on("resize", function () {
+		checkWidth();
 	});
 });
